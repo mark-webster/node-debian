9c1b3c9ed9970295060cb1591af8aacd2ce511f4
mark-webster <mark.webster@gmail.com>
2013-03-07 01:32:11 +0000

    Automatic changelog version. Updated default to v0.8.22

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION=0.8.21
+VERSION=0.8.22
 node_url=http://nodejs.org/dist/v${VERSION}/node-v${VERSION}.tar.gz
 node_tar=node-v${VERSION}.tar.gz
 node_dir=nodejs-${VERSION}
@@ -24,6 +24,7 @@ if [ ! -d "$node_dir/src" ]; then
 	tar zxf "$node_tar" --strip-components=1 -C "$node_dir"
 	echo "Creating $node_dir/debian ..."
 	cp -a deb "$node_dir/debian"
+	sed -e "s/\${VERSION}/${VERSION}/" deb/changelog > "$node_dir/debian/changelog"
 
 	if [ -z "$patchfile" ] && [ -f "$patchfile" ]; then
 		echo "Patching ..."
